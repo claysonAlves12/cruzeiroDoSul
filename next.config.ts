c8d07dff3477b599ff2f6cf2419189edae49861c
Dev principal <pedidosonline01@lojasnossolar.com.br>
2024-11-18 17:37:49 -0300

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,19 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  reactStrictMode: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '**', // Permite todos os domínios com HTTPS
+      },
+      {
+        protocol: 'http',
+        hostname: '**', // Permite todos os domínios com HTTP
+      },
+    ],
+  },
 };
 
 export default nextConfig;
